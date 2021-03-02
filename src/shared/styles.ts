452bdc2d946fc00de2995aaf27d333f7eff88e7c
Jake joo <joogeunjae@gmail.com>
2021-03-02 17:21:59 +0900

    add shared, includes colors and fonts

---
+++ src/shared/styles.ts
@@ -0,0 +1,64 @@
+export const dummy = {
+  sampleText: "과연 어디까지 자신이 성장할 수 있는지, 맘껏 도전하기 바란다.",
+};
+
+export const color = {
+  // solid
+  primary100: "#df001a",
+  primary90: "#f8001d",
+  primary80: "#ff122e",
+  primary70: "#ff2c45",
+  primary60: "#FF455B",
+  primary: "#FF455B",
+  primary50: "#ff5f71",
+  primary40: "#ff7888",
+  primary30: "#ff929e",
+  primary20: "#ffacb5",
+  primary10: "#ffc5cb",
+
+  // basic
+  black: "#000000",
+  white: "#ffffff",
+
+  // shade
+  gray100: "#121619",
+  gray90: "#21272a",
+  gray80: "#343a3f",
+  gray70: "#4d5358",
+  gray60: "#697077",
+  gray50: "#878d96",
+  gray40: "#a2a9b0",
+  gray30: "#c1c7cd",
+  gray20: "#dde1e6",
+  gray10: "#f2f4f8",
+};
+
+export const typography = {
+  family:
+    '-apple-system, BlinkMacSystemFont, "Helvetica Neue", Helvetica, Arial, sans-serif',
+  weight: {
+    thin: "100",
+    extralight: "200",
+    light: "300",
+    regular: "400",
+    medium: "500",
+    semibold: "600",
+    bold: "700",
+    extrabold: "800",
+    black: "900",
+  },
+  size: {
+    h1: 96,
+    h2: 64,
+    h3: 48,
+    h4: 36,
+    h5: 24,
+    h6: 20,
+    s1: 16,
+    s2: 14,
+    b0: 18,
+    b1: 16,
+    b2: 14,
+    link: 16,
+  },
+};
