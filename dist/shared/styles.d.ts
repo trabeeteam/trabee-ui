8a3aa3501b7809dcb7ff56e122dcb8fdfabad342
Jake joo <joogeunjae@gmail.com>
2021-03-02 17:24:30 +0900

    update version 0.7.0

---
+++ dist/shared/styles.d.ts
@@ -0,0 +1,56 @@
+export declare const dummy: {
+    sampleText: string;
+};
+export declare const color: {
+    primary100: string;
+    primary90: string;
+    primary80: string;
+    primary70: string;
+    primary60: string;
+    primary: string;
+    primary50: string;
+    primary40: string;
+    primary30: string;
+    primary20: string;
+    primary10: string;
+    black: string;
+    white: string;
+    gray100: string;
+    gray90: string;
+    gray80: string;
+    gray70: string;
+    gray60: string;
+    gray50: string;
+    gray40: string;
+    gray30: string;
+    gray20: string;
+    gray10: string;
+};
+export declare const typography: {
+    family: string;
+    weight: {
+        thin: string;
+        extralight: string;
+        light: string;
+        regular: string;
+        medium: string;
+        semibold: string;
+        bold: string;
+        extrabold: string;
+        black: string;
+    };
+    size: {
+        h1: number;
+        h2: number;
+        h3: number;
+        h4: number;
+        h5: number;
+        h6: number;
+        s1: number;
+        s2: number;
+        b0: number;
+        b1: number;
+        b2: number;
+        link: number;
+    };
+};
